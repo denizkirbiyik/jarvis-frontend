1b19c5d8b8be9361458a9f5b500662a14d29585b
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2025-07-15 12:53:32 -0400

    what

--- jarvis-frontend/app.vue
+++ jarvis-frontend/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <WhatTheSigma/>
+    <NuxtLayout ><NuxtPage/></NuxtLayout>
   </div>
 </template>
--- jarvis-frontend/layouts/default.vue
+++ jarvis-frontend/layouts/default.vue
@@ -1,4 +1,5 @@
 <template>
-    <div>test
+    <div>
+        <slot />
     </div>
 </template>
