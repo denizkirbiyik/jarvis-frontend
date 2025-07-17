1d2a1ba20af6b09cf3a415b0cb82293afd978751
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2025-07-16 22:01:53 -0400

    fre

--- jarvis-frontend/app.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtLayout ><NuxtPage/></NuxtLayout>
-  </div>
-</template>
--- jarvis-frontend/composables/getQuizzes.ts
+++
@@ -1,4 +0,0 @@
-export default async function () {
-  const d = await useFetch("http://127.0.0.1:8000/api/question/1");
-  return d;
-}
